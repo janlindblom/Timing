9d6b7420a60d04e63ffe057b9ebd5a067b9b6afd
Jan Lindblom <lindblom.jan@gmail.com>
2011-09-09 13:57:43 +0200

    Adding sources.

---
+++ src/com/github/janlindblom/Timing/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.github.janlindblom.Timing;
+
+/**
+ * @author Jan Lindblom <lindblom.jan@gmail.com>
+ * @version 0.0.1
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
