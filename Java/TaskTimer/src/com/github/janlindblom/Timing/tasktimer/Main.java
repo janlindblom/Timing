68a846bc8c266e388ed6480d2570368b28a521e5
Jan Lindblom <lindblom.jan@gmail.com>
2011-09-21 11:44:59 +0200

    Initiating NetBeans project.

---
+++ Java/TaskTimer/src/com/github/janlindblom/Timing/tasktimer/Main.java
@@ -0,0 +1,16 @@
+package com.github.janlindblom.Timing.tasktimer;
+
+/**
+ *
+ * @author Jan Lindblom <lindblom.jan@gmail.com>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
